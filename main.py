42a99d4516369aa667311e8ead1f71f6835fc945
isyerim3406 <isyerim3406@gmail.com>
2025-09-04 09:50:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -177,7 +177,7 @@ async def run_bot():
         prev_close = cl
 
     await send_telegram_message(
-        f"Bot Başlatıldı!\n"
+        f"{CFG['BOT_NAME']} Başladı!**\n"
         f"Mod:{CFG['MODE']}\n"
         f"Sembol: {CFG['SYMBOL']}\n"
         f"Zaman Aralığı: {CFG['INTERVAL']}\n"
@@ -249,3 +249,4 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
